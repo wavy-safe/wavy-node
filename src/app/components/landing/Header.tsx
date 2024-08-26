f2f8fc8a06e085658ffcbee989ee48e17d6f7d94
ede-404 <eder077@proton.me>
2024-08-25 20:00:45 -0600

    shadcn ui
    Installation of shadcn modules and correction of the CSS on the landing page.

--- src/app/components/landing/Header.tsx
+++ src/app/components/landing/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
       <Image src="/wavyNode.svg" alt="WavyNode Logo" width={100} height={50} />
       <h1 className="text-3xl font-medium text-gray-800">WavyNode</h1>
       <button className="bg-gray-800 text-white text-sm py-1 px-3 rounded-full">
-        Platform
+       <> Platform</>
       </button>
     </header>
   );
