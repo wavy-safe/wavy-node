d15f7c16f23554a3eff2cfcf664b8ca8bd2fd595
ede-404 <eder077@proton.me>
2024-08-26 23:20:52 -0600

    components
    css responsive

--- src/app/components/landing/MainContent.tsx
+++ src/app/components/landing/MainContent.tsx
@@ -4,7 +4,7 @@ import Header from "./Header";
 export default function MainContent() {
   return (
     <main className="p-8">
-      <div className="grid grid-cols-2 md:grid-cols-2 gap-8 items-center max-w-7xl mx-auto">
+      <div className="flex flex-col items-center justify-center main-h-screen bg-gray-50">
         <div className="max-w-lg">
           <h2 className="text-2xl font-bold mb-2">
             Total monitoring of Tornado Cash, Blocked Wallets and Exploits
@@ -15,25 +15,25 @@ export default function MainContent() {
           </p>
         </div>
 
-        <div className="flex justify-center md:justify-end">
+        <div className="mt-6">
+          <FeatureBox
+            title="Accurate Detection of Connections with dApps"
+            description="We identify illicit funds and analyze their route to dApps, ensuring comprehensive, real-time monitoring."
+          />
+        </div>
+
+        <div className="text-center mt-8">
+          <h2 className="text-lg font-bold">
+            We warn of possible dangerous connections to dApps.
+          </h2>
+        </div>
+
+        <div className="flex justify-center md:justify-end mt-4 ">
           <div className="bg-gray-700 p-8 rounded-md text-center">
             <span className="text-white block text-sm font-bold">Networks</span>
-            <img src="arB.svg" alt="Networks Icon" className="mx-auto mt-2" />
+            <img src="ethL.svg" alt="Networks Icon" className="mx-1 mt-2" />
           </div>
         </div>
-      </div>
-
-      <div className="mt-6">
-        <FeatureBox
-          title="Accurate Detection of Connections with dApps"
-          description="We identify illicit funds and analyze their route to dApps, ensuring comprehensive, real-time monitoring."
-        />
-      </div>
-
-      <div className="text-center mt-8">
-        <h2 className="text-lg font-bold">
-          We warn of possible dangerous connections to dApps.
-        </h2>
       </div>
     </main>
   );
