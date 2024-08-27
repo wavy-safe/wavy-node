b7a545206283e22bd91df60d969d53ffc0564d3f
ede-404 <eder077@proton.me>
2024-08-26 23:20:52 -0600

    components
    css responsive

--- src/app/components/landing/Header.tsx
+++ src/app/components/landing/Header.tsx
@@ -1,12 +1,14 @@
-import Image from 'next/image';
+import Image from "next/image";
+import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center p-5 bg-white shadow">
+    <header className="flex flex-col md:flex-row justify-between items-center p-5 bg-white shadow-lg space-y-4 md:space-y-0 md:space-x-4">
       <Image src="/wavyNode.svg" alt="WavyNode Logo" width={100} height={50} />
-      <h1 className="text-3xl font-medium text-gray-800">WavyNode</h1>
-      <button className="bg-gray-800 text-white text-sm py-1 px-3 rounded-full">
-       <> Platform</>
+      <h1 className="text-3xl font-medium text-gray-800">Wavy Node</h1>
+      
+      <button className="bg-gray-800 text-white text-sm py-2 px-4 rounded-full hover:bg-gray-700 hover:shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
+        Alpha
       </button>
     </header>
   );
