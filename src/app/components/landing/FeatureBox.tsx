0b614d53e7066fb5f7979809b9ec570cc0390f4b
ede-404 <eder077@proton.me>
2024-10-30 03:11:03 -0600

    update

--- src/app/components/landing/FeatureBox.tsx
+++ src/app/components/landing/FeatureBox.tsx
@@ -1,15 +1,14 @@
 interface FeatureBoxProps {
   title: string;
-  description: string;
 }
 
-export default function FeatureBox({ title, description }: FeatureBoxProps) {
+export default function FeatureBox({ title }: FeatureBoxProps) {
   return (
     <div className="p-4 text-center">
       <h2 className="bg-gray-800 text-white text-center text-lg py-2 px-5 rounded-full inline-block">
         {title}
       </h2>
-      <p className="mt-2 text-gray-700">{description}</p>
+      <p className="mt-2 text-gray-700"></p>
     </div>
   );
 }
