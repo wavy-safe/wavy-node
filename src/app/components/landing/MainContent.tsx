ac70a3c7b1c178d8b14b2cb499966a8149b73f3b
ede-404 <eder077@proton.me>
2024-10-30 03:06:25 -0600

    update

--- src/app/components/landing/MainContent.tsx
+++ src/app/components/landing/MainContent.tsx
@@ -4,8 +4,10 @@ import Header from "./Header";
 export default function MainContent() {
   return (
     <main className="p-8">
-      <div className="flex flex-col items-center justify-center main-h-screen bg-gray-50">
-        <div className="max-w-lg">
+      {/* Container for the main content */}
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+        {/* Section for heading and introductory text */}
+        <div className="max-w-lg text-center">
           <h2 className="text-2xl font-bold mb-2">
             Stay Protected: Real-Time Exploit Detection and On-Chain Data
             Insights
@@ -19,20 +21,23 @@ export default function MainContent() {
           </p>
         </div>
 
+        {/* FeatureBox component */}
         <div className="mt-6">
           <FeatureBox title="Accurate Detection of Connections with dApps" />
         </div>
 
+        {/* Secondary heading */}
         <div className="text-center mt-8">
           <h2 className="text-lg font-bold">
             We warn of possible dangerous connections to dApps.
           </h2>
         </div>
 
-        <div className="flex justify-center md:justify-end mt-4 ">
+        {/* Networks section */}
+        <div className="flex justify-center md:justify-end mt-8">
           <div className="bg-gray-700 p-8 rounded-md text-center">
             <span className="text-white block text-sm font-bold">Networks</span>
-            <img src="ethL.svg" alt="Networks Icon" className="mx-1 mt-2" />
+            <img src="ethL.svg" alt="Networks Icon" className="mx-auto mt-2" />
           </div>
         </div>
       </div>
