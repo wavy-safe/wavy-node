3419787d183d47ca0ef7a7c7ccc098667cbe596d
Manuel Rivera <menu.rivera@gmail.com>
2024-12-23 09:00:34 -0600

    [ADD] -> /api/addresses/report endpoint

--- src/lib/ai/index.ts
+++ src/lib/ai/index.ts
@@ -3,20 +3,35 @@ import { mainAgent } from "./agents"
 import { IBlacklist } from "@/types/blacklist.type"
 import { IEvent } from "@/types/event.type"
 
+/**
+	* Generates a report for the given address using generative ai 
+*/
 export const generateReport = async (address: string) => {
 	const supabase = await createClient()
 
+	// get all malicious events from db for that address 
 	const { data: events } = await supabase.from('events')
 		.select()
 		.eq('address', address)
 		.returns<IEvent[]>()
 
 
+	// get where it has been blacklisted
 	const { data: blacklists } = await supabase.from('blacklists')
 		.select()
 		.eq('address', address)
 		.returns<IBlacklist[]>()
 
+	if (!blacklists && !events) return 'The address is clean'
+
+	// Will end up with the following structure:
+	// Malicious events:
+	// {malicious event 1}
+	// {malicious event 2}
+	//
+	// Blacklisted from:
+	// {blacklist 1}
+	// {blacklist 2}
 	let findings = ''
 
 	if (events) findings = events.reduce((prev, curr) => {
@@ -24,13 +39,9 @@ export const generateReport = async (address: string) => {
 		return `${prev}\n${JSON.stringify(event)}`
 	}, 'Malicious events:')
 
-	if (blacklists) findings = blacklists!.reduce((prev, curr) => {
+	if (blacklists) findings = blacklists.reduce((prev, curr) => {
 		return `${prev}\n${JSON.stringify(curr)}`
 	}, `${findings}\n\nBlacklisted from:`)
-
-	if (!blacklists && !events) return 'The address is clean'
-
-	console.log(findings)
 
 	const res = await mainAgent.run({
 		messages: [
