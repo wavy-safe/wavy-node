0341c2b56df9dbcb0502620f552e2c5234c1031c
ede-404 <eder077@proton.me>
2024-12-23 11:37:33 -0600

    [MOD] layout footer

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -16,11 +16,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       className={`${inter.className} min-h-screen bg-background flex flex-col`}
     >
       <Header>
+        <main className="flex-grow">{children}</main>
         <footer className="py-6 text-center text-sm text-muted-foreground">
           <a
             href="https://t.me/wavynode"
             target="_blank"
             rel="noopener noreferrer"
+            className="hover:underline"
           >
             @wavynode
           </a>
