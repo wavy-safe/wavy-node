100bcb09d29792587701d264f1f7967ffae70407
ede-404 <eder077@proton.me>
2024-12-30 06:11:09 -0600

    [MOD] New search  /address/page

--- src/app/dashboard/address/[address]/page.tsx
+++ src/app/dashboard/address/[address]/page.tsx
@@ -1,7 +1,7 @@
+import Link from "next/link";
 import StatusDashboard from "@/components/dashboard/StatusDashboard";
 import BalanceOverview from "@/components/dashboard/balance-overview";
 import ReportAI from "@/components/dashboard/report-ai";
-
 
 export default function AddressPage({
   params,
@@ -12,17 +12,24 @@ export default function AddressPage({
 
   return (
     <div className="container mx-auto py-6 space-y-8">
-      <h1 className="text-xl font-bold text-gray-800">Wallet Details</h1>
-      
+      <div className="flex justify-between items-center">
+        <h1 className="text-xl font-bold text-gray-800">Wallet Details</h1>
+        <Link
+          href="/dashboard"
+          className="px-4 py-2 text-white bg-[#1B2B3A] hover:bg-[#14222C] rounded-lg"
+        >
+          New Search
+        </Link>
+      </div>
+
       {/* Componente de estado */}
       <StatusDashboard address={address} />
-      
+
       {/* Componente de balance */}
       <BalanceOverview address={address} />
 
-    {/* Componente AI*/}
+      {/* Componente AI */}
       <ReportAI address={address} />
-
     </div>
   );
 }
