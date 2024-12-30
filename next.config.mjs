6b0a1ada921aba84c23f636cc1f9258ac1cc37fd
ede-404 <eder077@proton.me>
2024-12-30 10:36:45 -0600

    [MOD] next.config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone', // Habilita el despliegue optimizado para plataformas como Cloudflare
-  experimental: {
-    appDir: true, // Habilita el uso del nuevo sistema de directorios en Next.js
-  },
 };
 
 export default nextConfig;
