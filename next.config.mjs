a10f7112e9eb74611814eb960af95ce8b3132c98
Manuel Rivera <menu.rivera@gmail.com>
2025-01-07 00:48:22 -0600

    [ADD] -> analyze script

--- next.config.mjs
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone', // Habilita el despliegue optimizado para plataformas como Cloudflare
-};
-
-export default nextConfig;
