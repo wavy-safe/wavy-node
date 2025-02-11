3a5f25692055e0ec42d332effe519318cfd1711e
ede-404 <eder077@proton.me>
2025-02-10 21:20:01 -0600

    [ADD] header Authorization Privy

---
+++ src/lib/auth.ts
@@ -0,0 +1,37 @@
+import axios, { AxiosInstance, InternalAxiosRequestConfig } from "axios";
+import { getAccessToken } from "@privy-io/react-auth";
+
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+if (!API_BASE_URL) {
+  throw new Error("NEXT_PUBLIC_API_BASE_URL no está configurado en el archivo .env");
+}
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  async (config: InternalAxiosRequestConfig) => {
+    const accessToken = await getAccessToken();
+
+    if (accessToken) {
+      config.headers = config.headers || {};
+      config.headers["Authorization"] = `Bearer ${accessToken}`;
+      config.headers["x-prvy-auth"] = accessToken;
+
+      const appId = process.env.NEXT_PUBLIC_PRIVY_APP_ID;
+      if (appId) {
+        config.headers["x-app-id"] = appId;
+      }
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default axiosInstance;
