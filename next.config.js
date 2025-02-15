d8eb3ba4605cdc6b75764219a67c04b63678ebc7
Eder <eder077@proton.me>
2025-02-15 02:28:57 -0600

    Revert "[FIX] config cloudflare"
    This reverts commit 2a2bd7e81c80a2b654645401123ded52fffc0f16.

--- next.config.js
+++ next.config.js
@@ -1,40 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: "standalone",
-  
+	output: 'standalone', 
 	experimental: {
-	  webpackMemoryOptimizations: true,
-	  optimizeCss: true,
-	  nextScriptWorkers: true,
+	  webpackMemoryOptimizations: true 
 	},
-  
-	compress: true,
-  
-	webpack: (config, { isServer }) => {
-	  if (!isServer) {
-		config.optimization.splitChunks = {
-		  chunks: "all",
-		  minSize: 20000,
-		  maxSize: 240000,
-		};
-	  }
-  
-	  if (process.env.ANALYZE === "true") {
-		const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+	webpack: (config) => {
+	  
+	  if (process.env.ANALYZE === 'true') {
+		const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 		config.plugins.push(new BundleAnalyzerPlugin());
 	  }
-  
 	  return config;
-	},
-  
-	modularizeImports: {
-	  lodash: {
-		transform: "lodash-es/{{member}}",
-	  },
-	  moment: {
-		transform: "moment/{{member}}",
-	  },
-	},
+	}
   };
   
   module.exports = nextConfig;
